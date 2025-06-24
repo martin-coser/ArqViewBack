ab7117efa0eccacbaa6c7a7446c3bf5ff4b82793
ulisescerq <cerquattiulises@gmail.com>
2025-06-24 14:54:34 -0300

    cliente listo, localidad listo

--- src/localidad/dto/update-localidad.dto.ts
+++ src/localidad/dto/update-localidad.dto.ts
@@ -3,6 +3,7 @@ import { CreateLocalidadDto } from './create-localidad.dto';
 import { IsInt, IsNotEmpty, IsString, IsOptional } from 'class-validator';
 import { Provider } from '@nestjs/common';
 import { Provincia } from 'src/provincia/entities/provincia.entity';
+import { Type } from 'class-transformer';
 
 export class UpdateLocalidadDto extends PartialType(CreateLocalidadDto) {
 
@@ -18,6 +19,7 @@ export class UpdateLocalidadDto extends PartialType(CreateLocalidadDto) {
     
     @IsNotEmpty()
     @IsOptional()
+    @Type(() => Number)
     provincia?: Provincia; // Clave foránea a provincia, se mantiene opcional para permitir actualizaciones parciales.
 
 }
