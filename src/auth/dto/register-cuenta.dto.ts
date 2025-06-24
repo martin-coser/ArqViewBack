10dd0ab5ea5e46508238b262d6e2fa30e0693d72
ulisescerq <cerquattiulises@gmail.com>
2025-06-24 15:23:44 -0300

    decoradores auth

--- src/auth/dto/register-cuenta.dto.ts
+++ src/auth/dto/register-cuenta.dto.ts
@@ -1,9 +1,22 @@
+import { IsEmail, IsNotEmpty, IsString, Matches, MinLength } from "class-validator"
+
 export class RegisterCuentaDto {
 
+    @IsString()
+    @IsNotEmpty()
     nombreUsuario:string
 
+    @IsString()
+    @IsNotEmpty()
+    @IsEmail()
     email:string
 
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(8, { message: 'La contraseña debe tener al menos 8 caracteres.' })
+    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, {
+            message: 'La contraseña debe contener al menos una mayúscula, una minúscula, un número y un carácter especial (@$!%*?&).'
+        })
     password:string
 
     rol: 'ADMIN' | 'CLIENTE' | 'INMOBILIARIA'
