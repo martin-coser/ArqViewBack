ef58228393358f91d22bbdaea79bb458b09d35da
HAriass <142324433+HAriass@users.noreply.github.com>
2025-07-08 19:04:53 -0300

    ya se pueden eliminar imagenes

--- src/imagen2d/imagen2d.controller.ts
+++ src/imagen2d/imagen2d.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, UploadedFile, UseInterceptors, Body } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseInterceptors, Body, Delete, HttpCode, HttpStatus, Param } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Imagen2dService } from './imagen2d.service';
 import { UploadImagen2dDto } from './dto/upload-imagen2d.dto';
@@ -15,4 +15,11 @@ export class Imagen2dController {
   ): Promise<{ imageUrl: string }> {
     return this.imagen2dService.upload(file, uploadImagen2dDto);
   }
+
+  @Delete('/remove/:id')
+  @HttpCode(HttpStatus.NO_CONTENT) // Retorna 204 No Content en caso de éxito
+  async deleteImage(@Param('id') id: number): Promise<void> {
+    await this.imagen2dService.remove(id);
+  }
+
 }
--- src/imagen2d/imagen2d.service.ts
+++ src/imagen2d/imagen2d.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Imagen2d } from './entities/imagen2d.entity';
 import { UploadImagen2dDto } from './dto/upload-imagen2d.dto';
+import * as fs from 'fs'; // Importa el módulo 'fs' para operaciones de archivos
+import * as path from 'path'; // Importa el módulo 'path' para resolver rutas
 
 @Injectable()
 export class Imagen2dService {
@@ -24,5 +26,35 @@ export class Imagen2dService {
 
     // Devolver la URL de la imagen
     return { imageUrl: savedImagen.filePath };
+  }
+
+  async remove(id: number): Promise<void> {
+    const imagen = await this.imagen2dRepository.findOneBy({ id });
+
+    console.log(`filepath desde el servicio ${imagen?.filePath}`)
+
+    if (!imagen) {
+      throw new NotFoundException(`Imagen con ID ${id} no encontrada.`);
+    }
+
+    // Construir la ruta absoluta del archivo
+    const imagePath = path.join(process.cwd(), imagen.filePath); // process.cwd() obtiene el directorio de trabajo actual
+
+    console.log(`ruta absoluta de la imagen ${imagePath}`)
+
+
+    // Eliminar el archivo del sistema de archivos
+    try {
+      if (fs.existsSync(imagePath)) { // Verificar si el archivo existe antes de intentar eliminarlo
+        fs.unlinkSync(imagePath);
+      }
+    } catch (error) {
+      console.error(`Error al eliminar el archivo ${imagePath}:`, error);
+      // Puedes decidir si quieres lanzar una excepción o simplemente registrar el error
+      // throw new InternalServerErrorException('Error al eliminar el archivo de imagen.');
+    }
+
+    // Eliminar la entrada de la base de datos
+    await this.imagen2dRepository.remove(imagen);
   }
 }
