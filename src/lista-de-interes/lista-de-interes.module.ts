e958271e3f2fbce3b1387e97aa1866b251bd896a
ulisescerq <cerquattiulises@gmail.com>
2025-07-10 16:14:10 -0300

    lista de interes module

--- src/lista-de-interes/lista-de-interes.module.ts
+++ src/lista-de-interes/lista-de-interes.module.ts
@@ -3,12 +3,18 @@ import { ListaDeInteresService } from './lista-de-interes.service';
 import { ListaDeInteresController } from './lista-de-interes.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ListaDeInteres } from './entities/lista-de-interes.entity';
+import { Propiedad } from 'src/propiedad/entities/propiedad.entity';
+import { Cliente } from 'src/cliente/entities/cliente.entity';
 
 @Module({
    imports: [
-      TypeOrmModule.forFeature([ListaDeInteres]),
+      TypeOrmModule.forFeature([
+        ListaDeInteres,
+        Propiedad,
+        Cliente
+      ]),
     ],
   controllers: [ListaDeInteresController],
-  providers: [ListaDeInteresService],
+  providers: [ListaDeInteresService,],
 })
 export class ListaDeInteresModule {}
