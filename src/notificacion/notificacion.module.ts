5405545f7a00383ee3bc3b65ba7e760c53127088
HAriass <142324433+HAriass@users.noreply.github.com>
2025-07-15 21:27:38 -0300

    envio de notificacion funcionando

--- src/notificacion/notificacion.module.ts
+++ src/notificacion/notificacion.module.ts
@@ -7,11 +7,35 @@ import { Propiedad } from 'src/propiedad/entities/propiedad.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { ListaDeInteres } from 'src/lista-de-interes/entities/lista-de-interes.entity';
+import { NotificacionMensaje } from './entities/notificacionMensaje.entity';
+import { Mensaje } from 'src/mensaje/entities/mensaje.entity';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigModule, ConfigService } from '@nestjs/config'; // Importar ConfigModule
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Cliente,Notificacion,Propiedad,ListaDeInteres]),
-EventEmitterModule
-],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // Cargar variables de entorno
+    TypeOrmModule.forFeature([Cliente, Notificacion, Propiedad, ListaDeInteres, NotificacionMensaje, Mensaje]),
+    EventEmitterModule.forRoot(), // Configuración recomendada para EventEmitter
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        transport: {
+          host: configService.get<string>('MAIL_HOST', 'smtp.gmail.com'),
+          port: configService.get<number>('MAIL_PORT', 587),
+          secure: false,
+          auth: {
+            user: configService.get<string>('MAIL_USER'), // grupo8albasoft@gmail.com
+            pass: configService.get<string>('MAIL_PASS'), // Contraseña o app password
+          },
+        },
+        defaults: {
+          from: '"No Reply" <grupo8albasoft@gmail.com>', // Asegúrate de que coincida con MAIL_USER
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [NotificacionController],
   providers: [NotificacionService],
 })
