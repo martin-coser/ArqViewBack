16b79ec116bbe8e8978d55c0f91cd894df7b6109
martin-coser <cosermartin@hotmail.com>
2025-09-09 14:51:51 -0300

    pullParaHernan

--- src/decoradores/roles.decorator.ts
+++ src/decoradores/roles.decorator.ts
@@ -1,4 +1,4 @@
 import { SetMetadata } from '@nestjs/common';
 
-export const ROLES_KEY = 'roles'; // Cambié 'role' a 'roles' para reflejar que es un array
+export const ROLES_KEY = 'roles'; // Cambié 'role' a 'roles' para reflejar que es un arrays
 export const Roles = (...roles: string[]) => SetMetadata(ROLES_KEY, roles);
