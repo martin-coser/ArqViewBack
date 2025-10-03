e0b92d0d7e3c6296ee3c53747cf3f365951b0ded
martin-coser <cosermartin@hotmail.com>
2025-10-03 15:32:40 -0300

    cambioEmails

--- src/notificacion/notificacion.module.ts
+++ src/notificacion/notificacion.module.ts
@@ -26,12 +26,12 @@ import { Cuenta } from 'src/auth/entities/cuenta.entity';
           port: configService.get<number>('MAIL_PORT', 587),
           secure: false,
           auth: {
-            user: configService.get<string>('MAIL_USER'), // grupo8albasoft@gmail.com
-            pass: configService.get<string>('MAIL_PASS'), // Contraseña o app password
+            user: configService.get<string>('MAIL_USER'), 
+            pass: configService.get<string>('MAIL_PASS'), 
           },
         },
         defaults: {
-          from: '"No Reply" <grupo8albasoft@gmail.com>', // Asegúrate de que coincida con MAIL_USER
+          from: '"No Reply" <arqview8@gmail.com>', 
         },
       }),
       inject: [ConfigService],
